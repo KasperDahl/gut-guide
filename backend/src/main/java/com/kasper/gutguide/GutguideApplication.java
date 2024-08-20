463c492be1a8d7df80658d7b55846fd63ce3b279
KasperDahl <76221662+KasperDahl@users.noreply.github.com>
2024-08-20 16:29:08 +0200

    first commit - Angular and Spring Boot setup

---
+++ backend/src/main/java/com/kasper/gutguide/GutguideApplication.java
@@ -0,0 +1,13 @@
+package com.kasper.gutguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutguideApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kasper/gutguide/GutguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasper.gutguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
