092f60df8d98979f574cf5d6230d342cf5867680
KasperDahl <76221662+KasperDahl@users.noreply.github.com>
2024-09-22 17:40:05 +0200

    Created Recipe-details component and routed it to recipes as links

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'recipes', component: RecipesComponent },
+    { path: 'recipe/:name', component: RecipeDetailsComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirect to home on empty path
 ];
--- frontend/src/app/services/recipe.service.ts
+++ frontend/src/app/services/recipe.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Recipe } from '../models/recipe.interface';
+import { Recipe } from '../models/recipe.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,14 +14,19 @@ export class RecipeService {
   constructor(private http: HttpClient) {}
 
   getRecipes(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.recipesUrl).pipe(
-      map((recipes) =>
-        recipes.map((recipe) => ({
-          name: recipe.name,
-          mealType: recipe.mealType,
-          timeToCook: recipe.timeToCook,
-        })),
-      ),
+    return this.http.get<Recipe[]>(this.recipesUrl);
+  }
+
+  getRecipeByName(name: string): Observable<Recipe> {
+    return this.getRecipes().pipe(
+      map(recipes => {
+        const foundRecipe = recipes.find(recipe => recipe.name === name);
+        if (!foundRecipe) {
+          throw new Error(`Recipe with name ${name} not found`);
+        }
+        return foundRecipe;
+      })
     );
   }
+  
 }
