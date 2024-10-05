a558d75481643630a4b2f97e0e6b191b00bf4d86
KasperDahl <76221662+KasperDahl@users.noreply.github.com>
2024-10-05 23:18:25 +0200

    Reorganized global styles, started on add-recipes component

--- frontend/src/app/components/add-recipe/add-recipe.component.ts
+++ frontend/src/app/components/add-recipe/add-recipe.component.ts
@@ -1,52 +1,149 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatOption } from '@angular/material/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-recipe',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatOption,
+    MatSelect,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardActions,
+  ],
   templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.scss']
+  styleUrls: ['./add-recipe.component.scss'],
 })
 export class AddRecipeComponent {
-  recipeForm: FormGroup;
+  recipeForm: FormGroup = this.fb.group({
+    name: [''],
+    instructions: this.fb.array(['']),
+    ingredients: this.fb.array([this.createIngredient()]),
+    servings: [null],
+    mealType: [''],
+    fullMeal: [false],
+    calories: [null],
+    timeToCook: [null],
+    comments: [''],
+    source: [''],
+    tried: [false],
+  });
 
-  constructor(private fb: FormBuilder) {
-    this.recipeForm = this.fb.group({
+  constructor(private fb: FormBuilder) {}
+
+  createIngredient(): FormGroup {
+    return this.fb.group({
       name: [''],
-      instructions: this.fb.array([]),
-      ingredients: this.fb.array([]),
-      servings: [''],
-      mealType: [''],
-      fullMeal: [''],
-      tried: [''],
-      comments: ['']
+      quantity: [null],
+      unit: [''],
+      quantityString: [''],
     });
   }
 
-  get instructions(): FormArray {
-    return this.recipeForm.get('instructions') as FormArray;
-  }
-
-  get ingredients(): FormArray {
-    return this.recipeForm.get('ingredients') as FormArray;
-  }
-
-  addInstruction(): void {
-    this.instructions.push(new FormControl(''));
-  }
-
-  addIngredient(): void {
-    this.ingredients.push(this.fb.group({
-      name: [''],
-      quantity: [''],
-      unit: ['']
-    }));
-  }
-
-  saveRecipe(): void {
-    console.log(this.recipeForm.value);
-  }
-
-  discardRecipe(): void {
-    this.recipeForm.reset();
-  }
+  // import { Component, OnInit } from '@angular/core';
+  // import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+  
+  // @Component({
+  //   selector: 'app-add-recipe',
+  //   templateUrl: './add-recipe.component.html',
+  //   styleUrls: ['./add-recipe.component.scss']
+  // })
+  // export class AddRecipeComponent implements OnInit {
+  //   recipeForm: FormGroup;
+  //   mealTypes = ['Breakfast', 'Lunch', 'Dinner', 'Snack'];
+  
+  //   constructor(private fb: FormBuilder) {
+  //     this.createForm();
+  //   }
+  
+  //   ngOnInit(): void {}
+  
+  //   createForm(): void {
+  //     this.recipeForm = this.fb.group({
+  //       name: [''],
+  //       instructions: this.fb.array(['']),
+  //       ingredients: this.fb.array([this.createIngredient()]),
+  //       servings: [null],
+  //       mealType: [''],
+  //       fullMeal: [false],
+  //       calories: [null],
+  //       timeToCook: [null],
+  //       comments: [''],
+  //       source: [''],
+  //       tried: [false]
+  //     });
+  //   }
+  
+  //   createIngredient(): FormGroup {
+  //     return this.fb.group({
+  //       name: [''],
+  //       quantity: [null],
+  //       unit: [''],
+  //       quantityString: ['']
+  //     });
+  //   }
+  
+  //   get instructions(): FormArray {
+  //     return this.recipeForm.get('instructions') as FormArray;
+  //   }
+  
+  //   get ingredients(): FormArray {
+  //     return this.recipeForm.get('ingredients') as FormArray;
+  //   }
+  
+  //   addInstruction(): void {
+  //     this.instructions.push(this.fb.control(''));
+  //   }
+  
+  //   removeInstruction(index: number): void {
+  //     this.instructions.removeAt(index);
+  //   }
+  
+  //   addIngredient(): void {
+  //     this.ingredients.push(this.createIngredient());
+  //   }
+  
+  //   removeIngredient(index: number): void {
+  //     this.ingredients.removeAt(index);
+  //   }
+  
+  //   onSubmit(): void {
+  //     if (this.recipeForm.valid) {
+  //       console.log(this.recipeForm.value);
+  //       // Here you would typically call a service to save the recipe
+  //     }
+  //   }
+  
+  //   onDiscard(): void {
+  //     this.recipeForm.reset();
+  //     // Reset arrays to initial state
+  //     while (this.instructions.length > 1) {
+  //       this.instructions.removeAt(1);
+  //     }
+  //     while (this.ingredients.length > 1) {
+  //       this.ingredients.removeAt(1);
+  //     }
+  //   }
+  // }
 }
