7080a2cd6a11d99ff9a7d73603dd03cca3d2c858
KasperDahl <76221662+KasperDahl@users.noreply.github.com>
2024-10-05 20:52:54 +0200

    AddRecipeComponent skeleton setup and routed to

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 import { AboutComponent } from './components/about/about.component';
+import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'recipes', component: RecipesComponent },
     { path: 'recipe/:name', component: RecipeDetailsComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'add-recipe', component: AddRecipeComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirect to home on empty path
 ];
