4e070027ec21d0ace7e30890a925bf1c68e182f3
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2026-01-11 16:55:06 +0100

    Revamp UX/ CSS + minor navbar and home component changes

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+import { RecipesComponent } from '../recipes/recipes.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [
+        RouterModule,
+        MatIconModule,
+        RecipesComponent,
+    ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
